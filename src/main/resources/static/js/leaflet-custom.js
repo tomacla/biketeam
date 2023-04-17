ea8c21f0e3973d670df3fc635f3d8dcd890e482b
tomacla <thomas.clarisse@gmail.com>
2023-04-17 10:53:11 +0200

    Change stamen to carto basemap

--- src/main/resources/static/js/leaflet-custom.js
+++ src/main/resources/static/js/leaflet-custom.js
@@ -43,6 +43,10 @@ var layers = {
         minZoom: 0,
         maxZoom: 20,
         ext: 'png'
+     }),
+     "Carto" : L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+     	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+     	maxZoom: 20
      })
 };
 
