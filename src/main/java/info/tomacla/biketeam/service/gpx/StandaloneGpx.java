ff3670963ef7200661eefc645e3d38a468a2a30c
Gabriel Landais <gabriel.landais@gmail.com>
2024-09-05 17:08:30 +0200

    Show climbs on all pages

--- src/main/java/info/tomacla/biketeam/service/gpx/StandaloneGpx.java
+++ src/main/java/info/tomacla/biketeam/service/gpx/StandaloneGpx.java
@@ -5,15 +5,11 @@ public class StandaloneGpx {
     private final double length;
     private final double positiveElevation;
     private final double negativeElevation;
-    private final String elevationProfile;
-    private final String geojson;
 
-    public StandaloneGpx(double length, double positiveElevation, double negativeElevation, String elevationProfile, String geojson) {
+    public StandaloneGpx(double length, double positiveElevation, double negativeElevation) {
         this.length = length;
         this.positiveElevation = positiveElevation;
         this.negativeElevation = negativeElevation;
-        this.elevationProfile = elevationProfile;
-        this.geojson = geojson;
     }
 
     public double getLength() {
@@ -28,11 +24,4 @@ public class StandaloneGpx {
         return negativeElevation;
     }
 
-    public String getElevationProfile() {
-        return elevationProfile;
-    }
-
-    public String getGeojson() {
-        return geojson;
-    }
 }
